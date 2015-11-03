7c0098f51c3b4506ef26d5ad61ea54e457294807
max <msteing@gmail.com>
2015-11-03 11:06:32 -0500

    idk next update

---
+++ device-server/node-server-local.js
@@ -0,0 +1,466 @@
+var express = require('express');
+
+// var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+// var mongoUrl = "mongodb://127.0.0.1:27017/uiradar";
+var mongoose = require('mongoose');
+var mongo_url = 'mongodb://127.0.0.1:3001/meteor';  // development db
+
+var db;
+
+var options = {
+  server: {},
+  replset: {}
+};
+options.server.socketOptions = options.replset.socketOptions = { keepAlive: 1};
+
+var deviceSchema = mongoose.Schema({
+  device_id: Number,
+  users: [{ user_id: Number }],
+  location: String
+});
+var Device = mongoose.model('Device', deviceSchema);
+
+var trajectorySchema = mongoose.Schema({
+  device_id: Number,
+  x: Number, 
+  y: Number, 
+  z: Number,
+  // x_coord: Number,
+  // y_coord: Number,
+  // z_coord: Number,
+  timestamp: String
+});
+var Trajectory = mongoose.model('Trajectory', trajectorySchema);
+
+var byteoffset = 0;
+var chunk_size = 41640;
+
+var traj_offset = 41600;
+var traj_size = 12
+
+var id_offset = 41612;
+var id_size = 4;
+
+var time_offset = 41632;
+var time_size = 8;
+
+var trajectoryObj = {};
+var counter = 0;
+
+trajectoryObj.id = counter;
+
+var xmin = -5.0;
+var xmax = 5.0;
+var ymin = 0.0;
+var ymax = 10.0;
+var zmin = 0.0;
+var zmax = 2.0;
+
+
+// trajectory data offset=41600 and size=12
+// num_dev offset=41612 and size=4
+// file_name offset=41624 and size=7
+// time offset=41616 and size=8
+// timestamp offset=41632 and size=8
+// Chunk size: 41640
+
+
+
+
+
+// var connectToDB = function(url, callback) {
+//   MongoClient.connect(url, function(err, db) {
+//     // assert.equal(null, err);
+//     // return db;
+//     callback(err, db);
+//   });
+// }
+
+// var handleDBConnection = function(err, db) {
+//   if (err === null) {
+//     console.log("connected to db");
+//     return db;
+//   }
+//   console.log(err);
+// }
+
+
+var app = express();
+
+//app.use(app.router);
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+// const default_http_port = 3000;
+// const http_port = process.env.PORT || default_http_port
+// app.listen(http_port);
+// console.log("Listening on port: " + http_port);
+
+
+// var WebSocketServer = require('ws').Server;
+// var wss = new WebSocketServer({ server:  app});
+
+// var clientId = 0;
+// wss.on('connection', function connection(ws) {
+//   var thisId = client++;
+//   console.log('WS client #%d connected', thisId);
+//   var location = url.parse(ws.upgradeReq.url, true);
+//   // you might use location.query.access_token to authenticate or share sessions 
+//   // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
+  
+//   // ws.on('message', function incoming(message) {
+//   //   console.log('received: %s', message);
+//   // });
+
+//   // ws.on('close', function() {
+//   // 	console.log("closing client #%d", thisId);
+//   // });
+ 
+//   // ws.send('something');
+// });
+
+
+var insertDocument = function(db, trajectory, device_id, ts) {
+   db.collection('trajectories').insertOne( {
+      "trajectory" : {
+         "x_coord" : trajectory.x,
+         "y_coord" : trajectory.y,
+         "z_coord" : trajectory.z
+      },
+      "device_id" : dev,
+      "timestamp" : ts,
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the trajectories collection.");
+    // callback(result);
+    return result;
+  });
+};
+
+
+/* Takes callback of form function(hasData, dataOffset)
+ * where hasData is a boolean and dataOffset is an int
+ */
+function containsData(byteoffset, data_offset, data_size, chunk_size, callback) {
+      // if traj data falls in this buffer
+    if (byteoffset <= data_offset && byteoffset + chunk_size > data_offset) {
+      
+      // if the buffer contains all the traj data
+      if (byteoffset + chunk_size > data_offset + data_size) {
+        // trajectory = {};
+
+        var offset = data_offset - byteoffset;
+
+        // var x = data.readFloatLE(offset);
+        // var y = data.readFloatLE(offset + 4);
+        // var z = data.readFloatLE(offset + 8);
+
+        // trajectory.x = x;
+        // trajectory.y = y;
+        // trajectory.z = z;
+
+        // callback(true, offset);
+        return offset;
+
+        // num_dev offset=41612 and size=4
+        // file_name offset=41624 and size=7
+        // time offset=41616 and size=8
+        
+
+        // console.log(trajectory);
+
+      } else {  // otherwise, the data gets cut off
+        console.log("data cut off");
+        // callback(false, offset);
+        return offset;
+      }
+
+    }
+    // callback(false, null);
+    return false;
+}
+
+function generateRandomNumber(min, max) {
+    var r = Math.random() * (max - min) + min;
+    return r;
+};
+
+function handleTrajectoryData(data, offset) {
+  trajectory = {};
+
+  var x = data.readFloatLE(offset);
+  var y = data.readFloatLE(offset + 4);
+  var z = data.readFloatLE(offset + 8);
+
+  // console.log("received trajectories(%d,%d,%d)", % )
+  // x = generateRandomNumber(xmin, xmax);
+  // y = generateRandomNumber(ymin, ymax);
+  // z = generateRandomNumber(zmin, zmax);
+
+  trajectory.x = x;
+  trajectory.y = y;
+  trajectory.z = z;
+
+  // console.log("coordinates", trajectory);
+  // callback(trajectory);
+  return trajectory;
+}
+
+function handleFlatField(data, offset) {
+  var field = data.readInt32LE(offset);
+  console.log(field);
+  return field;
+}
+
+
+
+function handleTimestamp(data, offset) {
+  // var timestamp = data.readInt32LE(offset);
+  var timestamp = data.toString(offset);
+  console.log("timestamp", timestamp)
+  return timestamp;
+}
+
+function handleDeviceId(data, offset) {
+  var deviceId = data.readInt32LE(offset);
+  // console.log("device_id", deviceId)
+  return deviceId;
+}
+
+
+
+
+var tls = require('tls');
+var fs = require('fs');
+
+var port = 8080;
+
+var options = {
+  // key: fs.readFileSync('server-key.pem'),
+  // cert: fs.readFileSync('server-cert.pem'),
+  key: fs.readFileSync('./certificates/server.key'),
+  cert: fs.readFileSync('./certificates/server.crt'),
+
+
+  // This is necessary only if using the client certificate authentication.
+  // requestCert: true,
+  // rejectUnauthorized: false
+
+  // This is necessary only if the client uses the self-signed certificate.
+  // ca: [ fs.readFileSync('client-cert.pem') ]
+  // secureProtocol: "SSLv3_method"
+};
+
+function checkBufferContains(currentByteOffset, memberByteOffset, memberSize, bytes) {
+	if ((currentByteOffset<=memberByteOffset) && (currentByteOffset+bytes > memberByteOffset)) {  		
+  		if (currentByteOffset+bytes > memberByteOffset+memberSize) {
+  			console.log("bystream contains member");
+  		}
+  	}
+}
+
+function readBufferData(data, Trajectory, callback) {
+  bytes = data.length;
+
+  console.log("reading", bytes, "from buffer at byteoffset", byteoffset);
+
+  var traj_start = containsData(byteoffset, traj_offset, traj_size, bytes);
+  
+  // var coords; 
+  console.log("trajectory data starts at", traj_start);
+
+  var trajectory_data = {};
+
+  if (traj_start !== false) {
+      var coordinates = handleTrajectoryData(data, traj_start);
+      trajectory.x = coordinates.x;
+      trajectory.y = coordinates.y;
+      trajectory.z = coordinates.z;
+      // trajectory_data.x = coords.x;
+      // trajectory_data.y = coords.y;
+  }
+
+
+  var id_start = containsData(byteoffset, id_offset, id_size, bytes);
+  if (id_start !== false) {
+      trajectory_data.device_id = handleDeviceId(data, id_start);
+  }
+
+  // var trajectory_data = {};
+  // trajectory_data.x = traj.x;
+  // trajectory_data.y = traj.y;
+  // trajectory
+
+  // var ts = containsData(byteoffset, time_offset, time_size, bytes);
+  // if (time_start !== false) {
+  //     ts = handleTimestamp(data, time_start);
+  // }
+
+  // callback(trajectory_data, Trajectory, bytes);
+  
+  // trajectory = new Trajectory({ device_id: device_id, coordinates: {x: traj.x, y: traj.y, z: traj.z}, timestamp: String(new Date().getTime())});
+  // trajectory.save(function(err, trajectory) {
+  //   if (err) return console.log(err);
+  //   console.log('saved successfully');
+  // })
+
+  // byteoffset += bytes;
+}
+
+function insertTrajectoryData(trajectory, Trajectory, bytes) {
+  var trajectoryDoc = new Trajectory({ device_id: trajectory.device_id, coordinates: {x: trajectory.x, y: trajectory.y, z: trajectory.z}, timestamp: String(new Date().getTime())});
+  trajectoryDoc.save(function(err, trajectoryDoc) {
+    if (err) return console.log(err);
+    console.log(trajectory, "saved successfully");
+    updateByteOffset(bytes);
+  })
+
+}
+
+function updateByteOffset(bytes) {
+    byteoffset += bytes;
+}
+
+
+var server = tls.createServer(options, function(socket) {
+
+  
+   
+    // db.once("open", function (callback) {
+    //   console.log("db connection opened");
+    // });
+
+    // var device1 = new Device({ device_id: 1, users: [{user_id: 100, user_id: 101}]});
+    // device1.save(function(err, device1) {
+    //   if (err) return console.log(err);
+    //   console.log('saved successfully');
+    // })
+
+
+
+    // var chunkInProgress = true; q
+
+    socket.addListener("data", function (data) {
+      
+      bytes = data.length;
+      // console.log("reading", bytes, "from buffer at byteoffset", byteoffset);
+
+      var traj, 
+          device_id, 
+          time_start;
+
+      var traj_start = containsData(byteoffset, traj_offset, traj_size, bytes);
+      if (traj_start !== false) {
+          var coordinates = handleTrajectoryData(data, traj_start);
+          trajectoryObj.x = coordinates.x;
+          trajectoryObj.y = coordinates.y;
+          trajectoryObj.z = coordinates.z;
+      }
+
+
+      var id_start = containsData(byteoffset, id_offset, id_size, bytes);
+      if (id_start !== false) {
+          device_id = handleDeviceId(data, id_start);
+          trajectoryObj.deviceId = device_id;
+      }
+
+      // // var ts = containsData(byteoffset, time_offset, time_size, bytes);
+      // // if (time_start !== false) {
+      // //     ts = handleTimestamp(data, time_start);
+      // // }
+      
+      // trajectory = new Trajectory({ device_id: device_id, coordinates: {x: traj.x, y: traj.y, z: traj.z}, timestamp: String(new Date().getTime())});
+      // trajectory.save(function(err, trajectory) {
+      //   if (err) return console.log(err);
+      //   console.log('saved successfully');
+      // })
+
+
+      byteoffset += bytes;
+
+      // readBufferData(data, Trajectory, insertTrajectoryData);
+
+      // console.log(byteoffset);
+      if (byteoffset > chunk_size) {
+        console.log("buffer overlaps on new chunk");
+      }
+
+      byteoffset = byteoffset % chunk_size;
+
+      if (byteoffset == 0) {
+        var timestamp = "" + new Date().getTime();
+        trajectoryObj.timestamp = timestamp;        
+        // console.log();
+        // console.log(trajectoryObj);
+
+        // TODO: use timestamp from device instead of setting it here
+
+        var trajectoryDoc = new Trajectory({ device_id: trajectoryObj.deviceId, x: trajectoryObj.x, y: trajectoryObj.y, z: trajectoryObj.z, timestamp: trajectoryObj.timestamp});
+        trajectoryDoc.save(function(err, trajectoryDoc) {
+          if (err) return console.log(err);
+          console.log();
+          console.log("saved successfully:", trajectoryDoc);
+            // updateByteOffset(bytes);
+        });
+
+        Trajectory.find({device_id: trajectoryObj.device_id}, function (err, trajectories) {
+          if (err) return console.log(err);
+          // console.log(trajectories.length);
+        });
+
+        trajectoryObj = {};
+        counter += 1;
+        trajectoryObj.id = counter;
+      }
+    // });
+
+    });
+  // });
+
+  
+  // if (!socket.authorized) {
+  //   console.log("socket unauthorized");
+  //   console.log(socket.authorizationError);
+  // }
+ 
+  // socket.pipe(socket);
+
+  socket.addListener('close', function (callback) {
+    console.log("socket connection closing");
+  });
+
+  socket.addListener('clientError', function (err, tlssocket) {
+    console.log("client error:", err);
+  });
+
+  socket.addListener('error', function (err) {
+    console.log("server error:", err);
+  });
+
+});
+
+// var io = require('socket.io')(server);
+
+// io.on('connection', function (socket) {
+//   console.log("socket connection");
+//   socket.on('data', function (data) {
+//     console.log("received data of size", data.length);
+//   })
+// })
+
+server.listen(port, function() {
+  console.log('server bound and listening on port', port);
+  mongoose.connect(mongo_url, options);
+  db = mongoose.connection;
+  db.on('error', function (err) {
+    console.error.bind(console, 'connection error:');
+  });
+  // db.once('open', function (callback) {
+  //   console.log('db opened');
+  //   console.log('server bound and listening on port', port);
+  // });
+});
--- web-server/meteor-uiradar/simple-todos.js
+++ web-server/meteor-uiradar/simple-todos.js
@@ -15,17 +15,28 @@ if (Meteor.isClient) {
 
 	Template.body.helpers({
 		trajectories: function() {
-			return Trajectories.find({device_id: 100, 
-																timestamp: {
-																	$gt: new Date(ISODate().getTime() - 1000 * 60 * .5)	// get only data from 30secs ago
-																}}, 
-																{sort: {timestamp: -1}});
+			Trajectories.find({device_id: 100});
+			// return Trajectories.find({device_id: 100, 
+			// 													timestamp: {
+			// 														$gt: Date.now() - 1000 * 60 * 10	// get only data from 30secs ago
+			// 													}}, 
+			// 													{sort: {timestamp: -1}});
 		},
 		trajectoryCount: function() {
 			return Trajectories.find({device_id: 100}).count();
 		},
 		deviceId: function() {
 			return 100;
+		},
+		recentTrajectoryCount: function() {
+			return Trajectories.find({device_id: 100, 
+																timestamp: {
+																	$gt: Date.now() - 1000 * 60 * 10 // get only data from 30secs ago
+																}}, 
+																{sort: {timestamp: -1}}).count(); 
+		},
+		timeDuration: function() {
+			return 30;
 		}
 	});
 
@@ -37,23 +48,40 @@ if (Meteor.isClient) {
 		var padding = 30;
 		
 		//Create scale functions
-		var xScale = d3.scale.linear()
-							 .range([padding, w - padding * 2]);
+		// var xScale = d3.scale.linear()
+		// 					 .range([padding, w - padding * 2]);
 
-		var yScale = d3.scale.linear()
-							 .range([h - padding, padding]);
+
+		// setup x 
+		var xValue = function(d) { return d.x;}, // data -> value
+		    xScale = d3.scale.linear().range([padding, w - padding * 2]), // value -> display
+		    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+		    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+		 // setup y
+		var yValue = function(d) { return d.y;}, // data -> value
+			  yScale = d3.scale.linear().range([h - padding, padding]), // value -> display
+			  yMap = function(d) { return yScale(yValue(d));}, // data -> display
+			  yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+
+		// var yScale = d3.scale.linear()
+		// 					 .range([h - padding, padding]);
+
+		// var xValue = function(d) { return d.x; };
+		// var yValue = function(d) { return d.y; }; 
 
 		//Define X axis
-		var xAxis = d3.svg.axis()
-						.scale(xScale)
-						.orient("bottom")
-						.ticks(10);
+		// var xAxis = d3.svg.axis()
+		// 				.scale(xScale)
+		// 				.orient("bottom")
+		// 				.ticks(10);
 
-		//Define Y axis
-		var yAxis = d3.svg.axis()
-						.scale(yScale)
-						.orient("left")
-						.ticks(10);
+		// //Define Y axis
+		// var yAxis = d3.svg.axis()
+		// 				.scale(yScale)
+		// 				.orient("left")
+		// 				.ticks(10);
 
 		//Create SVG element
 		var svg = d3.select("#xytrajectoryPlot")
@@ -66,22 +94,21 @@ if (Meteor.isClient) {
 		};
 
 		var opacity = function(d) {
-			var now = new Date(ISODate().getTime());
+			var now = new Date().getTime();
 			var diff = now - d.timestamp;
-
-		}
+		};
 
 		var opacity = d3.scale.ordinal()
-			.domain(timeRange())
-			.range([0.0, 1.0])
+			.domain([new Date().getTime() - 1000 * 60 * .5, new Date().getTime()])
+			.range([0.0, 1.0]);
 
 		var o = d3.scale.ordinal()
-			.domain(timeRange())
+			.domain([new Date().getTime() - 1000 * 60 * .5, new Date().getTime()])
 			.range(colorbrewer.RdBu[9]);
 
 		var timeRange = function() {
-			var now = new Date(ISODate().getTime());
-			var then = new Date(ISODate().getTime() - 1000 * 60 * .5);
+			var now = new Date().getTime();
+			var then = new Date().getTime() - 1000 * 60 * .5;
 			return [then, now];
 		}
 
@@ -95,29 +122,45 @@ if (Meteor.isClient) {
 
 		//Create X axis
 		svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + (h - padding) + ")")
-			.attr("line-width", 10);
+				.attr("class", "x axis")
+				.attr("line-width", 5)
+				.attr("transform", "translate(0," + (h - padding) + ")")
+				.call(xAxis)
+			.append("text")
+				.attr("class", "label")
+				.attr("x", w)
+				.attr("y", -6)
+				.style("text-anchor", "end")
+				.text("horizontal disance (meters)")
+
 		
 		//Create Y axis
 		svg.append("g")
-			.attr("class", "y axis")
-			.attr("transform", "translate(" + padding + ",0)");
+				.attr("class", "y axis")
+				.attr("transform", "translate(" + padding + ",0)")
+				.call(yAxis)
+			.append("text")
+	      .attr("class", "label")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("depth (meters)");
 
 		Deps.autorun(function(){
 			// var dataset = Trajectories.find({device_id: 100}, {sort:{timestamp:-1}}).fetch();
-			var data = Trajectories.find({device_id: 100, 
-																timestamp: {
-																	$gt: new Date(ISODate().getTime() - 1000 * 60 * .5)	// get only data from 30secs ago
-																}}, 
-																{sort: {timestamp: -1}});
-			// var dataset = Trajectories.find({device_id: 100}).fetch();
+			// var dataset = Trajectories.find({device_id: 100, 
+			// 													timestamp: {
+			// 														$gt: new Date().getTime() - 1000 * 60 * .5	// get only data from 30secs ago
+			// 													}}, 
+			// 													{sort: {timestamp: -1}});
+			var dataset = Trajectories.find({device_id: 100}).fetch();
 			
 			//Update scale domains
-			// xScale.domain([0, d3.max(dataset, function(d) { return d.x; })]);
-			// yScale.domain([0, d3.max(dataset, function(d) { return d.y; })]);
-			xScale.domain([-5, 5]);
-			yScale.domain([0, 10]);
+			// xScale.domain([d3.min(dataset, xValue)-1, d3.max(dataset, xValue)+1]);
+			// yScale.domain([d3.min(dataset, yValue)-1, d3.max(dataset, yValue)+1]);
+			xScale.domain([-6, 6]);
+			yScale.domain([-1, 11]);
 
 			//Update X axis
 			svg.select(".x.axis")
@@ -140,31 +183,36 @@ if (Meteor.isClient) {
 			circles
 				.enter()
 				.append("circle")
-				.attr("cx", function(d) {
-					return xScale(d.x);
-				})
-				.attr("cy", function(d) {
-					return yScale(d.y);
-				})
-				.attr("x", function(d) { return d.x })
-				.attr("y", function(d) { return d.y })
-				.attr("r", 3)
+				.attr("cx", xMap)
+				.attr("cy", yMap)
+				.attr("x", xValue)
+				.attr("y", yValue)
+				.attr("r", 8)
 				// .attr("fill", function(d) { return "rgb(0,0," + + ")"; })
-				.attr("fill", function(d) { return o(d.timestamp); })
+				.attr("fill", function(d) { return "red"; })
+				// .attr("fill", function(d) { return o(d.timestamp); })
 				// .attr("fill-opacity", function(d) { return d.timestamp; })
-				.attr("fill-opacity", function(d) { return opacity(d.timestamp); })
+				// .attr("fill-opacity", function(d) { return opacity(d.timestamp); })
+				.attr("fill-opacity", function(d) { return 1.0; })
 				.attr("timestamp", function(d) { return d.timestamp; });
 
 			// Update
 			circles
 				.transition()
-				.duration(1000)
-				.attr("cx", function(d) {
-					return xScale(d.x);
-				})
-				.attr("cy", function(d) {
-					return yScale(d.y);
-				});
+				.duration(5000)
+				.attr("fill", function() { return "blue"; })
+				.transition()
+				.duration(2000)
+				// .attr()
+				.attr("fill-opacity", function() { return 0.0; })
+				.remove();
+				// .attr("fill", function)
+				// .attr("cx", function(d) {
+				// 	return xScale(d.x);
+				// })
+				// .attr("cy", function(d) {
+				// 	return yScale(d.y);
+				// });
 
 			//Remove
 			circles
@@ -335,7 +383,7 @@ if (Meteor.isClient) {
 		// var dataset = Trajectories.find({device_id: 100},{sort:{timestamp:-1}}).fetch();
 		var dataset = Trajectories.find({device_id: 100, 
 																timestamp: {
-																	$gt: new Date(ISODate().getTime() - 1000 * 60 * .5)	// get only data from 30secs ago
+																	$gt: new Date().getTime() - 1000 * 60 * .5	// get only data from 30secs ago
 																}}, 
 																{sort: {timestamp: -1}});
 
